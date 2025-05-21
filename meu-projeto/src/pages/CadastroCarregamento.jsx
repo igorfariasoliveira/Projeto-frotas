4bb3927c4bbfba7bbad3a75bb828add5fdcd9812
igorfariasoliveira <igor10farias@gmail.com>
2025-05-21 10:54:33 -0300

    Criando pagina de cargas

--- meu-projeto/src/pages/CadastroCarregamento.jsx
+++ meu-projeto/src/pages/CadastroCarregamento.jsx
@@ -30,27 +30,50 @@ export default function CadastroCarregamento() {
 
   return (
     <DashboardLayout>
-    <div className="container">
-      <h1>Cadastro de Carregamento</h1>
-      <form onSubmit={handleSubmit} className="form-grid">
-        <input className="input" type="text" name="numeroCarregamento" placeholder="Número do Carregamento" value={formData.numeroCarregamento} onChange={handleChange} />
-        <input className="input" type="text" name="notasFiscais" placeholder="Notas Fiscais" value={formData.notasFiscais} onChange={handleChange} />
-        <input className="input" type="number" name="mdfe" placeholder="MDFe" value={formData.mdfe} onChange={handleChange} />
-        <input className="input" type="text" name="situacaoMdfe" placeholder="Situação do MDFe" value={formData.situacaoMdfe} onChange={handleChange} />
-        <input className="input" type="number" name="pesoTotal" placeholder="Peso Total" value={formData.pesoTotal} onChange={handleChange} />
-        <input className="input" type="date" name="dataSaida" placeholder="Data de Saída" value={formData.dataSaida} onChange={handleChange} />
-        <input className="input" type="date" name="dataPrevistaChegada" placeholder="Data Prevista de Chegada" value={formData.dataPrevistaChegada} onChange={handleChange} />
-        <input className="input" type="date" name="dataChegada" placeholder="Data de Chegada" value={formData.dataChegada} onChange={handleChange} />
-        <input className="input" type="number" name="kmInicial" placeholder="KM Inicial" value={formData.kmInicial} onChange={handleChange} />
-        <input className="input" type="number" name="kmFinal" placeholder="KM Final" value={formData.kmFinal} onChange={handleChange} />
-        <input className="input" type="number" name="totalKm" placeholder="Total KM" value={formData.totalKm} onChange={handleChange} />
-        <input className="input" type="text" name="destino" placeholder="Destino" value={formData.destino} onChange={handleChange} />
+      <div className="container">
+        <h1>Cadastro de Carregamento</h1>
+        <form onSubmit={handleSubmit} className="form-grid">
+          <label>Número do Carregamento</label>
+          <input className="input" type="text" name="numeroCarregamento" value={formData.numeroCarregamento} onChange={handleChange} />
 
-        <button type="submit" className="button-submit">
-          Salvar Carregamento
-        </button>
-      </form>
-    </div>
+          <label>Notas Fiscais</label>
+          <input className="input" type="text" name="notasFiscais" value={formData.notasFiscais} onChange={handleChange} />
+
+          <label>MDFe</label>
+          <input className="input" type="number" name="mdfe" value={formData.mdfe} onChange={handleChange} />
+
+          <label>Situação do MDFe</label>
+          <input className="input" type="text" name="situacaoMdfe" value={formData.situacaoMdfe} onChange={handleChange} />
+
+          <label>Peso Total</label>
+          <input className="input" type="number" name="pesoTotal" value={formData.pesoTotal} onChange={handleChange} />
+
+          <label>Data de Saída</label>
+          <input className="input" type="date" name="dataSaida" value={formData.dataSaida} onChange={handleChange} />
+
+          <label>Data Prevista de Chegada</label>
+          <input className="input" type="date" name="dataPrevistaChegada" value={formData.dataPrevistaChegada} onChange={handleChange} />
+
+          <label>Data de Chegada</label>
+          <input className="input" type="date" name="dataChegada" value={formData.dataChegada} onChange={handleChange} />
+
+          <label>KM Inicial</label>
+          <input className="input" type="number" name="kmInicial" value={formData.kmInicial} onChange={handleChange} />
+
+          <label>KM Final</label>
+          <input className="input" type="number" name="kmFinal" value={formData.kmFinal} onChange={handleChange} />
+
+          <label>Total KM</label>
+          <input className="input" type="number" name="totalKm" value={formData.totalKm} onChange={handleChange} />
+
+          <label>Destino</label>
+          <input className="input" type="text" name="destino" value={formData.destino} onChange={handleChange} />
+
+          <button type="submit" className="button-submit">
+            Salvar Carregamento
+          </button>
+        </form>
+      </div>
     </DashboardLayout>
   );
 }
